496fe014d8bab00b2f13f2c1ecd37ec0f1f0cffb
AlVal <105518873+valinok19@users.noreply.github.com>
2024-12-22 20:04:01 +0400

    Add files via upload

---
+++ src/components/ButtonDelete/index.jsx
@@ -0,0 +1,11 @@
+const ButtonDelete = ({deleteProduct, id}) => {
+    return (  
+        <button type="button" onClick={() => {
+            deleteProduct(id);
+        }}>
+            <img src="./img/icons/cross.svg" alt="Delete"></img>
+        </button>
+    );
+}
+ 
+export default ButtonDelete;
